54afa1e2bfe0352356378f4aaa259d82a49a254e
mrsami2k50 <mrsami2k50@gmail.com>
2026-01-13 14:30:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import yfinance as yf
 import matplotlib
-matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
